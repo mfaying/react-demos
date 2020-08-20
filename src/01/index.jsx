f77ade7169503569ef2f96c89bcb2f21468b2e65
mfaying <1213560387@qq.com>
2020-08-20 15:17:09 +0800

    feat

--- src/01/index.jsx
+++ src/01/index.jsx
@@ -5,7 +5,9 @@ import ReactDom from "react-dom";
 // 编写一个组件
 class App extends Component {
   render() {
+    // const和var类似，它是ES6中的变量命名关键字，它命名的是一个常量。
     const text = "test";
+    // jsx语法
     return <div>{text}</div>;
   }
 }
